411e621f15cd393c6367cf6194770453f0fd152c
Sarthak Jain <sarthak@carefi.in>
2024-11-02 21:08:42 +0530

    changed in vercel config

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -6,11 +6,10 @@ DEBUG = False
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-ALLOWED_HOSTS = [".vercel.app", ".now.sh"]
+ALLOWED_HOSTS = ["*"]
 
 STATIC_URL = os.path.join(BASE_DIR, "static")
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
-
 
 DATABASES = {"default": dj_database_url.config()}
 
