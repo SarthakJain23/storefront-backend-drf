3ff1f80eb140622cc82294390966e77c975fadcd
Sarthak Jain <sarthak@carefi.in>
2024-11-03 14:57:15 +0530

    Revert "alter static urkls"
    This reverts commit 68ea3f36e7213083ff7f63d0ccb839d8e7fb2a62.

--- build_files.sh
+++ build_files.sh
@@ -3,4 +3,5 @@ python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
+gunicorn storefront.wsgi
 echo "BUILD ENDED...."
