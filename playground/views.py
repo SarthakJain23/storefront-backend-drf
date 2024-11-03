e64023fd1c98ac66411db411e386a9ea56ffe309
Sarthak Jain <sarthak@carefi.in>
2024-11-03 12:55:55 +0530

    changed import method

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,10 @@
+import logging
+
+import requests
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
-from requests import get
 from rest_framework.views import APIView
-import requests
-
-import logging
 
 logger = logging.getLogger(__name__)
 
@@ -16,7 +15,7 @@ class HelloView(APIView):
     def get(self, request):
         try:
             logger.info("Calling httpbin")
-            response = get("https://httpbin.org/delay/2")
+            response = requests.request("GET", "https://httpbin.org/delay/2")
             logger.info("Response received")
             data = response.json()
         except requests.ConnectionError:
